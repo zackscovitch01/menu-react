70f84e567b6eb108637097f35db0d56bcd6b4db1
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-29 06:37:47 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
+import { useState } from "react";
+import Title from "./Title";
+import menu from "./data";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newMenu = menu.filter((item) => item.category === category);
+    setMenuItems(newMenu);
+  };
+  return (
+    <main>
+      <section className="menu">
+        <Title text={"our menu"} />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
